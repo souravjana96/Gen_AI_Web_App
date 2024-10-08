424dd27dace70d1d6e80109924af3bd5af99a7ec
souravjana96 <souravjana13196@gmail.com>
2024-10-08 20:07:41 +0530

    Form is created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ function App() {
   return (
     <div className="main-container">
       <h1>Gen AI Web App by Vizuara AI</h1>
+
+      <div className="form-container">
+
+        <div>
+          <label >Enter Query:</label>
+          <input type="text" 
+          className='query-input' placeholder="enter placeholder"/>
+        </div>
+
+      </div>
     </div>
   );
 }
